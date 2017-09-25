ca7997bb0f50f12033d4b4c2ff505a15570a93d3
Kyle <kyle@cloudversify.com>
2017-09-24 22:14:25 -0700

    scss and uglify

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ var path = require('path');
 var BUILD_DIR = path.resolve(__dirname, 'dist/');
 var APP_DIR = path.resolve(__dirname, 'src/');
 
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
 var config = {
   entry: APP_DIR + '/index.tsx',
   output: {
@@ -12,14 +14,32 @@ var config = {
   },
   resolve: {
       // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
+      extensions: [".ts", ".tsx", ".js", ".json", ".css"]
   },
   module: {
     loaders: [
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
     ]
-  }
+  },
+  plugins: [
+    new UglifyJSPlugin()
+  ]
 };
 
 module.exports = config;
