0129d60187b5c73c9b1c7f0bad38a486732b30a2
Kyle Galvin <KyleG@firmglobal.com>
2017-12-01 14:08:52 -0800

    heroku setup

--- src/stores/store.tsx
+++ src/stores/store.tsx
@@ -13,7 +13,7 @@ export const initialState: IAppState = {
   }
 }
 
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || redux.compose;
+const composeEnhancers = window ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : redux.compose;
 import * as Reducers from "../reducers/reducer";
 export const Store: redux.Store<IAppState> = redux.createStore(
   Reducers.reducers,
