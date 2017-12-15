3bfe741234eac07aae88c679a2dbbfab0baa6ecf
kyle galvin <darkdigitaldream@gmail.com>
2017-12-14 21:28:04 -0800

    more server code. need to clean, configure, test

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const path = require('path')
 const express = require('express')
 
-module.exports = {
-  app: function () {
-    const app = express()
-    const indexPath = path.join(__dirname, './dist/index.html')
-    const publicPath = express.static(path.join(__dirname, './dist'))
+const app = express()
+const indexPath = path.join(__dirname, '/../index.html')
+const publicPath = express.static(path.join(__dirname, '../public'))
+app.use('/public', publicPath)
+app.get('/', function (_, res) { res.sendFile(indexPath) })
+const port = (process.env.PORT || 8080)
 
-    app.use('/public', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
 
-    return app
-  }
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
+  const config = require('../webpack.dev.config.js')
+  const compiler = webpack(config)
+
+  app.use(webpackHotMiddleware(compiler))
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }))
 }
+
+app.listen(port)
+console.log(`Listening at http://localhost:${port}`)
