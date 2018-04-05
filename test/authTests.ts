8ef272b9e560118414ccffd9464dfd993fea8206
kyle galvin <darkdigitaldream@gmail.com>
2018-04-04 18:53:22 -0700

    added some imports so test doesn't fail on coveralls

--- test/authTests.ts
+++ test/authTests.ts
@@ -1,6 +1,26 @@
 import { suite, test, slow, timeout } from "mocha-typescript";
+import * as actions from '../src/actions/auth';
+import * as authService from "../src/services/auth";
+import { IState as SignupState } from "../src/components/signup";
 
 @suite class AuthProviderTests {
   @test async canCreateUserAndLogin() {
+    actions.AuthActionTypes.SIGN_UP;
+    var signupState: SignupState = {
+      "username": "",
+      "email": "",
+      "firstName": "",
+      "lastName": "",
+      "password": "",
+      "altPassword": "",
+      "validUsername": true,
+      "validEmail": true,
+      "validPassword": true,
+      "passwordMatch": true
+    }
+    var signupState: SignupState
+    var action: actions.ISignupAction = { 
+      type: actions.AuthActionTypes.SIGN_UP, 
+      access_token: signupState } 
   }
 }
