158bf2f701791f468765f148c55ce7d474ab1b6f
kyle galvin <darkdigitaldream@gmail.com>
2018-04-10 19:25:10 -0700

    first test workiung

--- test/authTests.ts
+++ test/authTests.ts
@@ -1,5 +1,7 @@
 import { suite, test, slow, timeout } from "mocha-typescript";
 import 'isomorphic-fetch';
+import * as assert from "assert";
+import * as jwt from "jsonwebtoken";
 
 import * as actions from '../src/actions/auth';
 import * as userService from "../src/services/user";
@@ -27,8 +29,18 @@ import * as Store from "../src/stores/store";
     await Store.store.dispatch(userService.signup(signupData));
     const signupState = Store.store.getState();
     console.log("signupState: ", signupState);
+
+    const jwtData = jwt.decode(Store.store.getState().auth) as any;
+    await Store.store.dispatch(userService.getUser(jwtData.id));
     const newUser = Store.store.getState().user;
+    console.log("new user: ", newUser);
     await Store.store.dispatch(userService.del(newUser));
+    const deletedUser = Store.store.getState().user;
+
+    console.log('deleted user: ', deletedUser);
+
+    assert.equal("first",newUser.firstName);
+    assert.notEqual(0,newUser.id);
 
 
     //dispatch action. maybe rename access_token?
