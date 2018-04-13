ac1b01067ccc384b35daee7f39b24f5785ad71e3
Kyle Galvin <kyle.galvin@confirmit.com>
2018-04-13 14:16:34 -0700

    heroku swagger url

--- src/config/herokuDev.ts
+++ src/config/herokuDev.ts
@@ -3,5 +3,5 @@ export default {
     frontendDomain: 'https://sharifyr-frontend.herokuapp.com',
     logLevel: 'info',
     port: '0', //overwritten by environment variable on deploy
-    swaggerUrl: 'http://localhost:3000/swagger.json'
+    swaggerUrl: 'https://sharifyr-backend.herokuapp.com/swagger.json'
   }
